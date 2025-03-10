b2b70348b4f4a3930c2000d86c5dfb040cab0e19
Kevin.Huang <kevin9huangjiahui@gmail.com>
2025-03-10 21:40:12 +0700

    Create implementation of networkservice the URLSessionNetworkService that will use UrlSession to hit api

---
+++ SimpleItunesPlayer/Network/URLSessionNetworkService.swift
@@ -0,0 +1,40 @@
+//
+//  URLSessionNetworkService.swift
+//  SimpleItunesPlayer
+//
+//  Created by CMP2024008 on 10/03/25.
+//
+
+import Foundation
+
+class URLSessionNetworkService: NetworkService {
+    func request<T: Decodable>(
+        _ endpoint: Endpoint,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        guard let url = endpoint.url else {
+            completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: -1, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(T.self, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
