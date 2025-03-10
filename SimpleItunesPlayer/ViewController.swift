97766dd0db649c3327b62563d7af404034762ad2
Kevin.Huang <kevin9huangjiahui@gmail.com>
2025-03-10 21:19:14 +0700

    Initial Commit

---
+++ SimpleItunesPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleItunesPlayer
+//
+//  Created by CMP2024008 on 10/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
