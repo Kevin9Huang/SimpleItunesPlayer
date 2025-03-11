b79341e1653d60c3dc2907b45cec031e46cc34d5
Kevin.Huang <kevin9huangjiahui@gmail.com>
2025-03-11 11:12:55 +0700

    Add generic model song that will be used later on in tableview

---
+++ SimpleItunesPlayer/Model/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  SimpleItunesPlayer
+//
+//  Created by CMP2024008 on 11/03/25.
+//
+
+struct Song {
+    let name: String
+    let artist: String
+    let album: String?
+    let thumbnailUrl: String
+    let previewUrl: String
+}
